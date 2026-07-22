58a8fa1b98b712a09e3d9bb2707ae2191322cb67
Your Name <your-email@example.com>
2026-07-22 15:14:17 +0800

    change n_gpus_per_node

--- scripts/train_cav_gsm8k.sh
+++ scripts/train_cav_gsm8k.sh
@@ -149,7 +149,7 @@ python3 -m cav_rl.verl.main_cav_ppo \
     trainer.project_name="${project_name}" \
     trainer.experiment_name="${experiment_name}" \
     trainer.logger='["console","wandb"]' \
-    ++trainer.n_gpus_per_node=1 \
+    trainer.n_gpus_per_node=1 \
     trainer.nnodes=1 \
     ++actor_rollout_ref.rollout.n_gpus_per_node=1 \
     ++critic.n_gpus_per_node=1 \
