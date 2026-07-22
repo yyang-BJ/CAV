f82ea4775b6f537e11f4287918e3c000df676ed3
Your Name <your-email@example.com>
2026-07-22 13:22:21 +0800

    change megatron

--- src/cav_rl/verl/main_cav_ppo.py
+++ src/cav_rl/verl/main_cav_ppo.py
@@ -89,14 +89,9 @@ class CAVTaskRunner:
 
         if config.actor_rollout_ref.actor.strategy in {"fsdp", "fsdp2"}:
             assert config.critic.strategy in {"fsdp", "fsdp2"}
-            from verl.single_controller.ray import RayWorkerGroup
-            from verl.workers.fsdp_workers import AsyncActorRolloutRefWorker
 
-            use_legacy_worker_impl = config.trainer.get("use_legacy_worker_impl", "auto")
-            if use_legacy_worker_impl in ["auto", "enable"]:
-                from verl.workers.fsdp_workers import CriticWorker
-            else:
-                from verl.workers.roles import CriticWorker
+            from verl.single_controller.ray import RayWorkerGroup
+            from verl.workers.engine_workers import AsyncActorRolloutRefWorker, CriticWorker
 
             actor_rollout_cls = (
                 AsyncActorRolloutRefWorker
@@ -107,7 +102,7 @@ class CAVTaskRunner:
         elif config.actor_rollout_ref.actor.strategy == "megatron":
             assert config.actor_rollout_ref.actor.strategy == config.critic.strategy
             from verl.single_controller.ray.megatron import NVMegatronRayWorkerGroup
-            from verl.workers.megatron_workers import ActorRolloutRefWorker, AsyncActorRolloutRefWorker, CriticWorker
+            from verl.workers.engine_workers import ActorRolloutRefWorker, AsyncActorRolloutRefWorker, CriticWorker
 
             actor_rollout_cls = (
                 AsyncActorRolloutRefWorker
